3196dbdbbcc169252a7deb966fa023a503adf0ac
Raj Kumar Singh <143270207+rajkrsingh9@users.noreply.github.com>
2024-07-01 16:22:21 +0000

    var_testing

---
+++ 01_basics/01_var.js
@@ -0,0 +1,13 @@
+const accountId = 144543
+let accountEmail = "hi@gmail.com"
+var accountPassword = "12345"
+accountCity = "serampore"
+
+
+//accountId = 2  // not allowed for constant
+
+accountEmail = "he@gmail.com"
+accountPassword = "00000"
+accountCity = "jaipur"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
